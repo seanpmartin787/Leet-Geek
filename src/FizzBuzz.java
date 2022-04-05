ad191e5a4a3becd4246b29d3e02887f18981e59e
Colin Mills <cgm71@georgetown.edu>
2022-04-04 22:10:04 -0500

    FizzBuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+
+        List<String> wordList = new ArrayList<>();
+
+        //define default initialization for a new HashMap
+        HashMap<Integer, String> codeDict =
+                new HashMap<Integer, String>() {
+            {
+                put(3, "Fizz");
+                put(5, "Buzz");
+            }
+        };
+
+
+        for (int i = 1; i <= n ; i++) {
+            String toAdd = "";
+
+            for (Integer key : codeDict.keySet()) {
+                if (i % key == 0) {
+                    toAdd += codeDict.get(key);
+                }
+            }
+            if (toAdd.isBlank()) {
+                toAdd = Integer.toString(i);
+            }
+            wordList.add(toAdd);
+        }
+
+        return wordList;
+    }
+}
