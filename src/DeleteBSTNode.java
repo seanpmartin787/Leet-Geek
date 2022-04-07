2ff931e1035e8691510436598b10dba69402e9f9
Colin Mills <cgm71@georgetown.edu>
2022-04-07 00:33:05 -0500

    Recursive

--- src/DeleteBSTNode.java
+++ src/DeleteBSTNode.java
@@ -5,89 +5,48 @@ import java.util.TreeMap;
 
 class DeleteBSTNode {
 
-      public class TreeNode {
-          int val;
-          TreeNode left;
-          TreeNode right;
-          TreeNode() {}
-          TreeNode(int val) { this.val = val; }
-          TreeNode(int val, TreeNode left, TreeNode right) {
-              this.val = val;
-              this.left = left;
-              this.right = right;
-          }
-      }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
 
-    public TreeNode deleteNode(TreeNode root, int key) {
-
-        if (root == null) {return root;}
-        else if (root.val == key && root.left == null && root.right == null ) {return null;}
-
-
-        TreeNode prev = root, next = root;
-
-        while (next != null && next.val != key) {
-            prev = next;
-            next = getNext(next, key);
-
+        TreeNode() {
         }
 
-        if (next == null) {
-            //Nothing to delete
-        } else {
-            if (next.val > prev.val) {
-                //right node
-                if (next.left == null && next.right ==null) {
-                    //if bottom node
-                    prev.right = null;
-                } else if (next.left == null || next.right == null) {
-                    //leaf node
-                    if (next.left != null) {
-                        prev.right = next.left;
-                    } else {
-                        prev.right = next.right;
-                    }
+        TreeNode(int val) {
+            this.val = val;
+        }
 
-                } else {
-                    next.val = removeInternal(next);
-                }//Internal node
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 
-            } else {
-                //right node
-                if (next.left == null && next.right ==null) {
-                    //if bottom node
-                    prev.left = null;
-                } else if (next.left == null || next.right == null) {
-                    //leaf node
-                    if (next.left != null) {
-                        prev.left = next.left;
-                    } else {
-                        prev.left = next.right;
-                    }
-                } else {
-                    next.val = removeInternal(next);
-                }//Internal node
-
-            }//left node
-        }//else we got a winner
-
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return root;
+        if(root.val < key) root.right = deleteNode(root.right, key);
+        else if(root.val > key) root.left = deleteNode(root.left, key);
+        else{
+            if(root.left == null) return root.right;
+            else if(root.right == null) return root.left;
+            else{
+                TreeNode newRoot = root.right, par = null;
+                while(newRoot.left != null){
+                    par = newRoot;
+                    newRoot = newRoot.left;
+                }
+                if(par == null){
+                    newRoot.left = root.left;
+                    return newRoot;
+                }
+                par.left = newRoot.right;
+                newRoot.left = root.left;
+                newRoot.right = root.right;
+                return newRoot;
+            }
+        }
         return root;
-
-    }
-    public TreeNode getNext(TreeNode prev, int Searchvalue) {
-
-        if (prev.val > Searchvalue) prev = prev.left;
-        else prev = prev.right;
-
-        return  prev;
-    }
-
-    public TreeNode getMin(TreeNode node) {
-
-        while (node.left != null) {
-            node = node.left;
-        }//find smallest node in right sub tree
-        return node;
-
     }
 }
