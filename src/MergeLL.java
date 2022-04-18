b2d16b795b8ccf1ccb0411b61005825fe4ea56bb
Colin Mills <cgm71@georgetown.edu>
2022-04-18 16:26:13 -0500

    eh

--- src/MergeLL.java
+++ src/MergeLL.java
@@ -1,29 +1,45 @@
+import javax.swing.text.ElementIterator;
+
 public class MergeLL {
-
-//    Definition for singly-linked list.
-public class ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
-
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head = list1;
-        ListNode temp = null;
 
-        return head;
-
-        while (list1 != null && list2 != null) {
-            if (list1.val == list2.val) {
-
-                temp = list2.next;
-                list2.next = list1.next.next;
-                list1.next = list2;
-            }
-
-
+        ListNode head;
+        if (list1 == null) {
+            return list2;
+        } else {
+            head = list1;
         }
+        ListNode temp;
+
+
+        while (list1 != null  && list2 != null) {
+            if (list1.val > list2.val) {
+                temp = list2.next;
+                list2.next = list1;
+                head = list1 = list2;
+                list2 = temp;
+            } else if (list1.next == null) {
+                list1.next = list2;
+                break;
+
+            } else if (list1.val == list2.val) {
+                temp = list2.next;
+                //insert node into list
+                list2.next = list1.next;
+                list1.next = list2;
+                //set list 2 back to the previous next for List 2
+                list2 = temp;
+            } else if (list1.val < list2.val && list1.next.val > list2.val) {
+                temp = list2.next;
+                //insert node into list
+                list2.next = list1.next;
+                list1.next = list2;
+                //set list 2 back to the previous next for List 2
+                list2 = temp;
+            } else {
+                list1 = list1.next;
+            }
+        }
+        return head;
     }
 }
