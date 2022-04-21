bd1cbe2410fdec18b783003f139b9047d713b1ef
Colin Mills <cgm71@georgetown.edu>
2022-04-20 19:29:00 -0500

    Recursion, but it sucks less

--- src/ClimbStairsRecursion.java
+++ src/ClimbStairsRecursion.java
@@ -1,13 +1,26 @@
 public class ClimbStairsRecursion {
+    public static void main(String[] args) {
+        int[] test = new int[8];
+        System.out.println(test[2]);
+    }
     public int climbStairs(int n) {
-        //base case
-        if ( n == 0 ) return 0;
-        if ( n == 1 ) return 1;
-        if ( n == 2 ) return 2;
 
-        n = climbStairs(n - 1) + climbStairs(n - 2);
+        int[] memo = new int[n + 1];
+        memo[0] = 0;
+        memo[1] = 1;
+        memo[2] = 2;
+        n = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
 
         return n;
+    }
+    private static int climbStairs(int n, int[] memo) {
+        //base case
+        if (n == 0) return 0;
+        if (memo[n] != 0) return memo[n];
+
+        int res = climbStairs(n-2, memo) + climbStairs(n-1, memo);
+        memo[n] = res;
+        return res;
     }
     /*
 
