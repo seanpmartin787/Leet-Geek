22de4e3495ba0f562fa2b1d0b9e977d12e03f5e5
Colin Mills <cgm71@georgetown.edu>
2022-04-21 18:18:10 -0500

    better solution

--- src/HammingWeight.java
+++ src/HammingWeight.java
@@ -2,17 +2,13 @@ public class HammingWeight {
     public static void main(String[] args) {
         System.out.println(hammingWeight(12));
         System.out.println();
-        long = 00100000000000000000000000000000;
     }
     public static int hammingWeight(int n) {
-        int[] bitArray = new int[n + 1];
-        bitArray[0] = 0;
-        bitArray[1] = 1;
-
-        for (int i = 2; i < bitArray.length; i++) {
-            if ((i & 1) == 0) bitArray[i] = bitArray[i >> 1];
-            else bitArray[i] = bitArray[i -1] + 1;
+        int ones = 0;
+        while(n!=0) {
+            ones += (n & 1);
+            n = n>>>1;
         }
-        return bitArray[bitArray.length - 1];
+        return ones;
     }
 }
