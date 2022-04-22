d97ac42b8cb54f34b3d83ce41e26a374e9d2a280
Colin Mills <cgm71@georgetown.edu>
2022-04-22 17:35:30 -0500

    flip em

---
+++ src/ReverseBits.java
@@ -0,0 +1,18 @@
+public class ReverseBits {
+    public static void main(String[] args) {
+        reverseBits(43261596);
+    }
+    public int reverseBits(int n) {
+        if (n == 0) return 0;
+
+        int flip = 0;
+        for (int i = 0; i < 32; i ++) {
+            flip <<= 1;
+            if ((n & 1) == 1) {
+                flip += 1;
+            }
+            n = n >> 1;
+        }
+        return flip;
+    }
+}
