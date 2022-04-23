f74867524c8e54fcd66b017e23e6b7bdd6920799
Colin Mills <cgm71@georgetown.edu>
2022-04-23 10:45:51 -0500

    stupid <=

--- src/SearchSortedMatrix.java
+++ src/SearchSortedMatrix.java
@@ -1,5 +1,9 @@
 public class SearchSortedMatrix {
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public static void main(String[] args) {
+        int[][] matrix = new int[][] {{1,3,5,7},{10,11,16,20},{23,30,34,50}};
+        searchMatrix(matrix, 20);
+    }
+    public static boolean searchMatrix(int[][] matrix, int target) {
         if (matrix.length == 0) return false;
         int low = 0, high = matrix.length - 1, midx = 0, midy = 0;
 
@@ -7,7 +11,7 @@ public class SearchSortedMatrix {
             midx = low + (high - low) / 2;
 
             if (matrix[midx][0] == target) return true;
-            else if (matrix[midx][0] < target && target < matrix[midx][matrix[midx].length - 1]) break; //we found the right x index
+            else if (matrix[midx][0] < target && target <= matrix[midx][matrix[midx].length - 1]) break; //we found the right x index
             else if (matrix[midx][0] > target) high = midx - 1;
             else low = midx + 1;
         }
