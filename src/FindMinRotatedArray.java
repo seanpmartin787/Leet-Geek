3f340d407d927e3ca3d069dc907b3f8d457000c1
Colin Mills <cgm71@georgetown.edu>
2022-04-23 11:36:04 -0500

    fix

--- src/FindMinRotatedArray.java
+++ src/FindMinRotatedArray.java
@@ -1,5 +1,4 @@
 public class FindMinRotatedArray {
-    public int findMin(int[] nums) {
         public int findMin(int[] nums) {
             if (nums.length == 0) return 0;
             if (nums.length == 1) return nums[0];
@@ -29,13 +28,13 @@ public class FindMinRotatedArray {
             return -1;
         }
 }
-        0,1,2,4,5,6,7
-        7,0,1,2,4,5,6
-        6,7,0,1,2,4,5
-        5,6,7,0,1,2,4
-        4,5,6,7,0,1,2
-        2,4,5,6,7,0,1
-        1,2,4,5,6,7,0
+//        0,1,2,4,5,6,7
+//        7,0,1,2,4,5,6
+//        6,7,0,1,2,4,5
+//        5,6,7,0,1,2,4
+//        4,5,6,7,0,1,2
+//        2,4,5,6,7,0,1
+//        1,2,4,5,6,7,0
 
 
 
