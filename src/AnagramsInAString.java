74904df0d81881d1179e2043e609bc18975dc2ca
Colin Mills <cgm71@georgetown.edu>
2022-04-27 19:53:13 -0500

    sliding door without hash table

--- src/AnagramsInAString.java
+++ src/AnagramsInAString.java
@@ -9,8 +9,8 @@ public class AnagramsInAString {
         System.out.println(findAnagrams(one,two));
     }
     public static List<Integer> findAnagrams(String s, String p) {
-        int startW = 0, matches = 0, sizeW = 0;
-        int[] charArray = new int[26];
+        int startW = 0, matches = 0;
+        int[] window = new int[26];
         int[] pFrequency = new int[26];
 
         List<Integer> res = new LinkedList<>();
@@ -21,22 +21,26 @@ public class AnagramsInAString {
 
 
         for (int i = 0; i < s.length(); i++) {
-            int count = pFrequency[s.charAt(i) - 'a'];
-            int end = ++charArray[s.charAt(i) - 'a'];
-            if (count > 0 && end <= count) matches++;
+            int freq = pFrequency[s.charAt(i) - 'a'];
+            int end = ++window[s.charAt(i) - 'a'];
+            if (freq > 0 && end <= freq) matches++;
 
-            //cbaebabacd
+            //if window is at max size
             if (i - startW + 1 == p.length()) {
+                //if we found a matching substring
                 if (matches == p.length()) res.add(startW);
 
-                int front = charArray[s.charAt(startW) - 'a']--;
-                if (count > 0 && front > 0)
+                //find out how many of the start char we have and then decrement
+                int front = window[s.charAt(startW) - 'a']--;
+                freq = pFrequency[s.charAt(startW) - 'a'];
+                //if we lose a match
+                // aka the number we have becomes less than the number we need of a char
+                if (freq > 0 && front <= freq)
                     matches--;
 
                 startW++;
             }
         }
-
         return res;
     }
 }
