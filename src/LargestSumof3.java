87fdebe9bc80f3bfed62862bf6ea1376adff82f0
Colin Mills <cgm71@georgetown.edu>
2022-04-28 16:54:47 -0500

    sliding door

---
+++ src/LargestSumof3.java
@@ -0,0 +1,21 @@
+public class LargestSumof3 {
+    public static void main(String[] args) {
+        System.out.println(largestSum(new int[]{6, 3, 7, 2, 4, 9}));
+    }
+    public static int largestSum (int[] s) {
+        int sum = 0, sizeW = 0, maxSum = 0;
+
+        for (int i = 0; i < s.length; i++) {
+             sum += s[i];
+             sizeW++;
+
+             if (sizeW == 3) {
+                 maxSum = Math.max(maxSum, sum);
+                 sum -= s[i - 2];
+                 sizeW--;
+             }
+
+        }
+        return maxSum;
+    }
+}
