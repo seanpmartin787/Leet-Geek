a56667752a3d2ab741c440ee9f9272041b4a720c
Colin Mills <cgm71@georgetown.edu>
2022-05-04 20:21:58 -0500

    almost there

---
+++ src/CombinationSumUnique.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSumUnique {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        backTrack(candidates, target,0,  new ArrayList<>(), res);
+        return res;
+    }
+    private static void backTrack (int[] nums, int remaining, int start, List<Integer> set, List<List<Integer>> res) {
+        if (remaining < 0) return; //sum was too large, not a match
+        if (remaining == 0) {
+            //we got a winner
+            res.add(new ArrayList<>(set));
+            return;
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+            set.add(nums[i]);
+            backTrack(nums, remaining - nums[i], i, set, res);
+            set.remove(set.size()-1);
+        }
+    }
+}
