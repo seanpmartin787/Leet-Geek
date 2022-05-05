9e78a80a892070bf764091e9413ecbcbf7e54d16
Colin Mills <cgm71@georgetown.edu>
2022-05-04 19:59:50 -0500

    permutations with dupes

--- src/PermutationsDupes.java
+++ src/PermutationsDupes.java
@@ -1,33 +1,44 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 public class PermutationsDupes {
-    public List<List<Integer>> permuteUnique(int[] nums) {
+    public static void main(String[] args) {
+        //System.out.println(permuteUnique(new int[] {1,2,3}));
+        //[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+        //System.out.println(permuteUnique(new int[] {1,1,2}));
+        //[[1,1,2],
+        // [1,2,1],
+        // [2,1,1]]
+//        System.out.println(permuteUnique(new int[] {1,1,1}));
+        System.out.println(permuteUnique(new int[] {3,3,0,3}));
+
+
+    }
+    public static List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         int[] visited = new int[nums.length];
+        Arrays.sort(nums);
 
-        backTrack(nums, visited, 0, new ArrayList<>(), res);
+        backTrack(nums, visited, new ArrayList<>(), res);
 
         return res;
 
     }
 
-    private static void backTrack (int[] nums, int[] visited, int start, List<Integer> path, List<List<Integer>> res) {
+    private static void backTrack (int[] nums, int[] visited, List<Integer> path, List<List<Integer>> res) {
 
         if (path.size() == nums.length) {
             res.add(new ArrayList<>(path));
+            return;
         }
 
-        for (int i = start; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
 
-            if (visited[i] == 1 || i > 0 && nums[i-1] == nums[i]) continue;
+            if (visited[i] == 1 || (i > 0 && nums[i-1] == nums[i] && visited[i-1] == 0)) continue;
 
             path.add(nums[i]);
             visited[i] = 1;
 
-            backTrack(nums,visited,i + 1, path, res);
+            backTrack(nums,visited, path, res);
 
             path.remove(path.size()-1);
             visited[i] = 0;
