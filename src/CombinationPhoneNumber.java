50c1f3055b09b2dd8c2a80b915fe2aafee35be3e
Colin Mills <cgm71@georgetown.edu>
2022-05-05 19:53:30 -0500

    I am humbled

--- src/CombinationPhoneNumber.java
+++ src/CombinationPhoneNumber.java
@@ -1,8 +1,7 @@
 import java.util.*;
 
 public class CombinationPhoneNumber {
-    HashMap<Character, char[]> candidates = new HashMap<>() {{
-        put('1',null);
+    private static final HashMap<Character, char[]> candidates = new HashMap<>() {{
         put('2',new char[] {'a','b','c'});
         put('3',new char[] {'d','e','f'});
         put('4',new char[] {'g','h','i'});
@@ -13,22 +12,27 @@ public class CombinationPhoneNumber {
         put('9',new char[] {'w','x','y','z'});
 
     }};
-    public List<String> letterCombinations(String digits) {
+
+    public static void main(String[] args) {
+        System.out.println(letterCombinations(""));
+    }
+    public static List<String> letterCombinations(String digits) {
         //classic backtracking problem
         //We want three things, a test for a desired state (digits.length())
         //a way to get our next candidates
         //and recursive backtrack function to build our states
         //To build our states we want an object to store solved states and a temp object to store temp states
         List<String> solvedStates = new ArrayList<>();
+        if (digits.equals(null) || digits.equals("")) return new ArrayList<String>();
         char[] state = new char[digits.length()];
-        CombinationPhoneNumber letterCombos = new CombinationPhoneNumber();
-        letterCombos.solveNumbers(digits, solvedStates, state, 0);
+        solveNumbers(digits, solvedStates, state, 0);
         return solvedStates;
 
     }
-    private void solveNumbers (String digits, List<String> solvedStates, char[] state, int start) {
-        if (state.length == digits.length()) {
-            solvedStates.add(state.toString());
+    private static void solveNumbers (String digits, List<String> solvedStates, char[] state, int start) {
+        if (start == digits.length()) {
+            solvedStates.add(new String(state));
+            return;
         }
 
         for (char candidate : candidates.get(digits.charAt(start))) {
