c4678cc6fe0069c6e51ba0ae28d598845e0149f9
Colin Mills <cgm71@georgetown.edu>
2022-05-07 15:26:40 -0500

    documentation

--- src/LongestPalindrome.java
+++ src/LongestPalindrome.java
@@ -9,17 +9,7 @@ public class LongestPalindrome {
 
     }
     public static String longestPalindrome(String s) {
-        //this feels like sliding window
-        //I should be always asking, can I do better than this?
-        //A palindrome requires the letter mirrored across the middle to be the same
-        // even number of items (Ex. (a,a), (a,b,b,a) etc.
-        // and odd number of items (Ex. (a,b,a), (a,b,c,b,a) etc.
-        // edge cases:
-        // -empty string
-        // - no palindrome (or longest palindrome == 1 ex. (a,b,c,d,e))
-        // - s.length == palindrome (ex. (a,a,a,a,a,a,a))
-
-        //let's do a bfs of any palindrome we find by seeing if start - 1 == end + 1
+        //looks like we basically did two DFSs at each index of the string
 
         int palindromeStart = 0, palindromeEnd = 0, maxPalindrome = 0;
         String longestPalindrome = "", substring = "";
@@ -32,7 +22,7 @@ public class LongestPalindrome {
             maxPalindrome = longestPalindrome.length();
             if (maxPalindrome == s.length()) break;
 
-            //look for even palidromes
+            //look for even palindromes
             substring = getLongestPalindrome(s, i, j+1);
             longestPalindrome = substring.length() > maxPalindrome ? substring : longestPalindrome;
             maxPalindrome = longestPalindrome.length();
