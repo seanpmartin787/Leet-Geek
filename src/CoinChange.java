f316546b0aeab2de616f310425321037d9350a70
Colin Mills <cgm71@georgetown.edu>
2022-05-13 16:30:32 -0500

    memo solution

--- src/CoinChange.java
+++ src/CoinChange.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class CoinChange {
     public static void main(String[] args) {
-        System.out.println(coinChange(new int[] {1,2,5},11));
+        System.out.println(coinChange(new int[] {5,2,1},11));
         System.out.println(coinChange(new int[] {1},0));
     }
     public static int coinChange(int[] coins, int amount) {
@@ -14,28 +15,22 @@ public class CoinChange {
         // then for each of those, lets all the coins that keep us below total
         // If one of those equals total we have a match, and we can return whichever level we were on
 
+        int[] memo = new int[amount+1];
         if (amount == 0) return 0;
+        getSums(coins,memo,amount);
+        return memo[amount];
+    }
+    private static int getSums (int[] coins, int[] memo, int amount) {
+        if (amount < 0) return Integer.MAX_VALUE - (memo.length); //return arbitrarily large number so it doesn't get picked
+        if (amount == 0) return 0; //we found a match, aka no need for more coins
+        if (memo[amount] != 0) return memo[amount];
 
-        Queue<Integer> BFS = new LinkedList<>();
-        int level = 1, size = 0;
-
-        for (int x : coins) {
-            if (x == amount) return level; //quick win
-            else if (x < amount ) BFS.offer(x);
+        //we have a few options, we can take the same coin, or any of the others
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int x: coins) {
+            minHeap.add(getSums(coins,memo,amount-x) + 1);
         }
-
-        while (!BFS.isEmpty()) {
-            level++;
-            size = BFS.size();
-            //This is clearly 2^N  growth, I bet we can do better
-            for (int i = 0; i < size; i++) {
-                int num = BFS.poll();
-                for (int x : coins) {
-                    if (x + num == amount) return level;
-                    else if (x + num < amount ) BFS.offer(x+num);
-                }
-            }
-        }
-        return -1;
+        memo[amount] = minHeap.poll();
+        return memo[amount];
     }
 }
