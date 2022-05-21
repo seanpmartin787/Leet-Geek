4da113e7e521c81ccd9a9c77539cb94d4c8db7ed
Colin Mills <cgm71@georgetown.edu>
2022-05-21 14:52:43 -0500

    extra memory, no destruction

---
+++ src/CopyLLWithRandom.java
@@ -0,0 +1,73 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CopyLLWithRandom {
+
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+
+    public Node copyRandomList(Node head) {
+        //I know this one
+        //We want to loop through the original list twice
+        //The first time we will create a copy of each node, and a copy of the next
+        //We will also add each of these new nodes to a HashMap for random pointer assignment
+        //Then we will loop through each list again, this time assigning the random pointers (can't do it off of value because of duplicate values
+        //edge cases:
+            //head is null: return null
+            //head is just one node: return new node
+            //all values are the same for unique nodes: we need to cache location of second list based on index, not value
+        if (head == null) return null;
+        //declare the variables we will need
+        Map<Integer,Node> newListMap = new HashMap();
+        Map<Node,Integer> randomIndexMap = new HashMap();
+        Node current = head, currentOther = null, newHead = null;
+        int i = 0;
+
+        while (current != null) {
+            //This will allow random access lookup for random pointer assignment
+            randomIndexMap.put(current, i++);
+            current = current.next;
+        }
+
+        //don't have to worry about null b/c of edge case checking
+        //intitialize new list
+        newHead = new Node(head.val);
+        currentOther = newHead;
+        newListMap.put(i++,currentOther);
+
+        //reset current and i
+        i=0;
+        current = head;
+        //copy list and cache indexes of otherList nodes
+        while (current != null && current.next != null) {
+            currentOther.next = new Node(current.next.val);
+            newListMap.put(i++,currentOther.next);
+            current = current.next;
+            currentOther = currentOther.next;
+        }
+
+        //reset currentOther and current
+        current = head;
+        currentOther = newHead;
+        //loop through once more to assign random pointers
+        //we will get the index of current's random pointer from randomIndexMap
+        //and then get the node at that index in new list with newListMap
+        while (current != null) {
+            if (current.random != null) currentOther.random = newListMap.get(randomIndexMap.get(current.random));
+            current = current.next;
+            currentOther = currentOther.next;
+        }
+        return newHead;
+    }
+}
