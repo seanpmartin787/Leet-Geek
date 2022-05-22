8875073acea12e1d07a996a399feafbbcc8ae021
Colin Mills <cgm71@georgetown.edu>
2022-05-22 14:33:07 -0500

    walker + runner, very smart

--- src/LLCycle.java
+++ src/LLCycle.java
@@ -1,5 +1,15 @@
+import java.util.HashSet;
+import java.util.List;
+
 public class LLCycle {
-    public class Solution {
+        class ListNode {
+            int val;
+            ListNode next;
+            ListNode(int x) {
+                val = x;
+                next = null;
+            }
+        }
         public boolean hasCycle(ListNode head) {
             while (head != null && head.val != Integer.MIN_VALUE) {
                 head.val = Integer.MIN_VALUE;
@@ -7,5 +17,24 @@ public class LLCycle {
             }
             return head != null;
         }
-    }
+
+        public boolean hasCycleSet(ListNode head) {
+            HashSet<ListNode> seen = new HashSet<>();
+            while (head != null && !seen.contains(head)) {
+                seen.add(head);
+                head = head.next;
+            }
+            return head != null;
+        }
+        public boolean hasCycleTwoPointer(ListNode head) {
+            if(head==null) return false;
+            ListNode walker = head;
+            ListNode runner = head;
+            while(runner.next!=null && runner.next.next!=null) {
+                walker = walker.next;
+                runner = runner.next.next;
+                if(walker==runner) return true;
+            }
+            return false;
+        }
 }
