9ce5e9a8f570541ca066aca285e927ee2198a687
Colin Mills <cgm71@georgetown.edu>
2022-06-01 20:54:50 -0500

    is permutation

--- src/Permutations.java
+++ src/Permutations.java
@@ -3,7 +3,26 @@ import java.util.HashSet;
 import java.util.List;
 
 public class Permutations {
+    public static void main (String[] args) {
+        System.out.println(isPermutation("abc","cb"));
+        System.out.println(isPermutation("abca","cba"));
+        System.out.println(isPermutation("abc","cba"));
+        System.out.println(isPermutation("abc","zzz"));
+    }
 
+    public static boolean isPermutation(String m, String n) {
+        if (m.length() != n.length()) return false;
+        var charArr = new int[26];
+
+        for (char ch: m.toCharArray()) charArr[ch - 'a']++;
+
+        for (int i=0; i < n.length();i++) {
+            charArr[n.charAt(i) - 'a']--;
+            if (charArr[n.charAt(i) - 'a'] < 0) return false;
+        }
+
+        return true;
+    }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> perms = new ArrayList<>();
         permute(perms, nums, new ArrayList<>(), new HashSet<>());
